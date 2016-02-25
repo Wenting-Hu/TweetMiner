2e58c11e2b8e4772021ed16fabf04125975e22e7
wh89@duke.edu <wh89@duke.edu>
2016-02-24 23:41:06 -0500

    Style

--- client/client.js
+++ client/client.js
@@ -1,18 +1,18 @@
 Data = new Meteor.Collection("data");
 
-if(Meteor.isClient) {
-    Meteor.startup(function() {
-        Meteor.setInterval(function() {
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+        Meteor.setInterval(function () {
             Meteor.call("updateRank");
-        },1000*15);
+        }, 1000 * 15);
 
-        Meteor.setInterval(function() {
+        Meteor.setInterval(function () {
             Meteor.call("updateMention");
-        }, 1000*15);
+        }, 1000 * 15);
 
         Meteor.call("refreshRandom");
 
-        Meteor.setInterval(function() {
+        Meteor.setInterval(function () {
             Meteor.call("updateTraffic");
         }, 1000);
 
@@ -20,24 +20,24 @@ if(Meteor.isClient) {
 
 
     Template.topic10.helpers({
-       tweets: function() {
-           var results = Data.find({type:"hashtag10"}).fetch()[0];
-           return results["entries"];
-       }
+        tweets: function () {
+            var results = Data.find({type: "hashtag10"}).fetch()[0];
+            return results["entries"];
+        }
 
     });
 
     Template.mention10.helpers({
-        tweets: function() {
-            var results =  Data.find({type:"mention"}).fetch()[0];
+        tweets: function () {
+            var results = Data.find({type: "mention"}).fetch()[0];
             return results["entries"];
         }
 
     });
 
     Template.random10.helpers({
-        tweets: function() {
-            var results =  Data.find({type:"random10"}).fetch()[0];
+        tweets: function () {
+            var results = Data.find({type: "random10"}).fetch()[0];
             return results["entries"];
         }
 
@@ -45,14 +45,25 @@ if(Meteor.isClient) {
 
 
     Template.refreshButton.events({
-         "click .buttonRefresh": function() {
-             console.log("click buttion");
-             Meteor.call("refreshRandom");
-         }
+        "click .buttonRefresh": function () {
+            console.log("click buttion");
+            Meteor.call("refreshRandom");
+        }
     });
 
+    Template.script_template.onRendered(function() {
+        $(document).ready(function() {
+            var script = document.createElement("script");
+            script.type="text/javascript";
+            script.src = "tweetspersec.js";
+            $("#script_div").append(script);
+        });
+    });
+
+
+
     Template.speedmeter.helpers({
-        traffic: function() {
+        traffic: function () {
             return Data.find({type: "traffic"}).fetch()[0]["traffic"];
         }
 
---
+++ public/tweetspersec.js
@@ -0,0 +1,47 @@
+/**
+ * Created by huwenting on 2/24/16.
+ */
+
+
+
+var svg = d3.select("#speedometer")
+    .append("svg:svg")
+    .attr("width", 400)
+    .attr("height", 400);
+
+
+var gauge = iopctrl.arcslider()
+    .radius(160)
+    .events(false)
+    .indicator(iopctrl.defaultGaugeIndicator);
+gauge.axis().orient("in")
+    .normalize(true)
+    .ticks(12)
+    .tickSubdivide(3)
+    .tickSize(10, 8, 10)
+    .tickPadding(5)
+    .scale(d3.scale.linear()
+        .domain([0, 4000])
+        .range([-3*Math.PI/4, 3*Math.PI/4]));
+
+var segDisplay = iopctrl.segdisplay()
+    .width(110)
+    .digitCount(4)
+    .negative(false)
+    .decimals(0);
+
+svg.append("g")
+    .attr("class", "segdisplay")
+    .attr("transform", "translate(150, 250)")
+    .call(segDisplay);
+
+svg.append("g")
+    .attr("class", "gauge")
+    .call(gauge);
+
+setInterval(update, 1000);
+
+function update() {
+    segDisplay.value(Data.find({type: "traffic"}).fetch()[0]["traffic"]);<!-- this is Tweets Per Second ------------------------------------------------------- -->
+    gauge.value(Data.find({type: "traffic"}).fetch()[0]["traffic"]);
+}
